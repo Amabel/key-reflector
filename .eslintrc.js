a813305be032c56eadabda8ee6561f3fcd0910a1
Amabel <luoweibinb@gmail.com>
2019-10-04 14:31:49 +0900

    chore: config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['google', 'prettier'],
+  extends: ['prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
