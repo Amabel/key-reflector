dd2d12fe092c7596ba3802a945289307bad550a3
Amabel <luoweibinb@gmail.com>
2019-10-04 18:15:31 +0900

    chore: add babel parser to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from 'rollup-plugin-babel'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import changeCase from 'change-case'
@@ -37,6 +38,11 @@ export default [
       file: pkg.module,
       format: 'esm',
     },
-    plugins: [nodeResolve()],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      nodeResolve(),
+    ],
   },
 ]
