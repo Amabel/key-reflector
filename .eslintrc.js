4337eb6d2ea2efbe409531d9eb1afe83f430f09a
Amabel <luoweibinb@gmail.com>
2019-10-04 10:09:09 +0900

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,9 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module',
-    parser: 'babel-eslint',
     ecmaVersion: 2015,
   },
 }
